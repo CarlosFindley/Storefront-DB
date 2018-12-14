4910b933f3a892c8e75ee5c9a200bf8f40f73296
Carlos Villalta <CarlosFindley@protonmail.com>
2018-12-14 01:10:49 -0800

    adding sql and js files

---
+++ storefront_db.sql
@@ -0,0 +1,52 @@
+-- if db exists, drop it --
+DROP DATABASE IF EXISTS bamazon;
+
+-- creates db --
+CREATE DATABASE bamazon;
+
+-- for the code below to use the db --
+USE bamazon;
+
+
+CREATE TABLE products (
+	-- create columns capable of holding all of the data contained within the db --
+    item_id INTEGER NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(50)  NOT NULL,
+    department_name VARCHAR(50) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INTEGER NOT NULL,
+    PRIMARY KEY (item_id)
+);
+
+-- Creates new rows containing data in all named columns --
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Chillies", "Produce", 1.00, 50000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Cacahuatl", "Produce", 10.00, 20000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Elotl", "Produce", .50, 100000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Octli", "Beverage", 5.00, 15000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Obsidian", "Weapon", 7.25, 1000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Jade", "Jewelry",  100.00, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Feathers", "Jewelry", 1.00, 5000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Canoe", "Navigation", 300.00, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Book", "Literature", 2.00, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Chillies", "Produce", 1.00, 3);
+
+SELECT * from products

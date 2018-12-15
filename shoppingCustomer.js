e0871d7716cf46ba2123c6471cebe1ea046d3888
Carlos Villalta <CarlosFindley@protonmail.com>
2018-12-15 02:32:45 -0800

    update

--- shoppingCustomer.js
+++ shoppingCustomer.js
@@ -20,8 +20,6 @@ connection.connect(function(err) {
     console.log("\n-=-=-=-=-=>~ Welcome to The Great Market of Tenochtitlán ~<=-=-=-=-=-");
     console.log("   ~<=-=-=-=-=-      Products From Téul to Izapa      -=-=-=-=-=>~   \n")
     displayInventory();
-    // End db connection
-    connection.end();
 });
  
 // To pull our inventory
@@ -30,6 +28,52 @@ function displayInventory() {
   connection.query("SELECT department_name, product_name, price, stock_quantity FROM products", function(err, results) {
     if (err) throw err;
     console.table(results);
+    userPurchase()
   });
 }
 
+// To purchase
+function userPurchase() {
+  connection.query("SELECT * FROM products", function (err, results) {
+    if (err) throw err;
+    // Once we have the items, prompt the user for which they'd like to buy
+    inquirer
+      .prompt([
+        {
+          message: "What would you like to buy (select from product_name)?",
+          type: "input",
+          name: "item"
+        },
+        {
+          message: "How many would you like to buy (select from stock_quantity)?",
+          type: "input",
+          name: "quantity"
+        }
+      ]).then(function(answer) {
+        var itemChoice = parseInt(parseInt(answer.item) -1);
+        var itemTotal = parseInt(answer.quantity) * results[itemChoice].price_per_item;
+
+        // Update database
+        if (parseInt(answer.quantity) <= results[itemChoice].stock_quantity) {
+          connection.query("UPDATE products SET ? WHERE",
+          [
+            {
+              stock_quantity: (parseInt(results[itemChoice].stock_quantity) - answer.quantity)
+            },
+            {item_id: answer.item}
+        ],
+      function (err) {
+        if (err) throw err;
+        console.log("Thanks for your order!  Your Total is " + "$" + itemTotal.toFixed(2));
+        console.log("\n\n");
+        // End db connection
+        connection.end();
+      });
+    } else {
+      console.log("Quantity has exeeded inventory, please lower the order quantity");
+
+      userPurchase();
+    }
+  });
+})
+};
--- storefront_db.sql
+++ storefront_db.sql
@@ -13,7 +13,7 @@ CREATE TABLE products (
     item_id INTEGER NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(50)  NOT NULL,
     department_name VARCHAR(50) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
+    price_per_item DECIMAL(10,2) NOT NULL,
     stock_quantity INTEGER NOT NULL,
     PRIMARY KEY (item_id)
 );
